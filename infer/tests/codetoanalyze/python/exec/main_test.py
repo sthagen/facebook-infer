f4c7ee13149eb1a42c8cdd4905851163dc05059c
Dulma Churchill <dulmarod@meta.com>
2026-02-27 04:27:46 -0800

    [llvm] Fix tests
    Reviewed By: davidpichardie
    
    Differential Revision: D94530300
    
    fbshipit-source-id: 3f6cac52accd9141ec13ebc5d3632e6c87e4bbb6

--- infer/tests/codetoanalyze/python/exec/main_test.py
+++ infer/tests/codetoanalyze/python/exec/main_test.py
@@ -5,12 +5,15 @@
 
 x = 42
 print(x)
+#stdout: 42
 # stdout: 42
 y = True
 print(y, None)
+#stdout: True None
 # stdout: True None
 y = False
 print(y, "hello world")
+#stdout: False hello world
 # stdout: False hello world
 
 
@@ -29,6 +32,7 @@ incr(3)
 incr(2)
 no_effect(-1)
 print("n =", n)
+#stdout: n = 5
 # stdout: n = 5
 
 
@@ -41,33 +45,41 @@ def fact(n):
 
 
 print("fact(5) =", fact(5))
+#stdout: fact(5) = 120
 # stdout: fact(5) = 120
 
 # basic import module
 import module1
 
 print("module1.f =", module1.f)
+#stdout: module1.f = module1.f
 # stdout: module1.f = module1.f
 module1.f = "explicitly modified from main"
 print("module1.f =", module1.f)
+#stdout: module1.f = explicitly modified from main
 # stdout: module1.f = explicitly modified from main
 module1.set("modified with a setter")
 print("module1.f =", module1.get())
+#stdout: module1.f = modified with a setter
 # stdout: module1.f = modified with a setter
 # testing recursive function + import
 print("module1.fact(5) =", module1.fact(5))
+#stdout: module1.fact(5) = 120
 # stdout: module1.fact(5) = 120
 from module1 import get as get1, set, fact as factorial
 
 set("modified with an imported setter")
 print("module1.f =", get1())
+#stdout: module1.f = modified with an imported setter
 # stdout: module1.f = modified with an imported setter
 from module1 import f as f_from_module1
 
 print("module1.f =", f_from_module1)
+#stdout: module1.f = modified with an imported setter
 # stdout: module1.f = modified with an imported setter
 # testing recursive function + from .. import
 print("factorial(5) =", factorial(5))
+#stdout: factorial(5) = 120
 # stdout: factorial(5) = 120
 
 # class body scope
@@ -86,24 +98,31 @@ class C:
 
 
 print("x is", C.get_x())
+#stdout: x is global
 # stdout: x is global
 x = "assigned by module body"
 print("x is", C.get_x())
+#stdout: x is assigned by module body
 # stdout: x is assigned by module body
 C.x = "assigned as a class attribute"
 print("x is", C.get_C_x())
+#stdout: x is assigned as a class attribute
 # stdout: x is assigned as a class attribute
 print("saved x is", C.saved_x)
+#stdout: saved x is global
 # stdout: saved x is global
 print(C.__name__)
+#stdout: C
 # stdout: C
 
 # Tuples
 l = (1, "1", (0, True))
 print(l)
+#stdout: (1, '1', (0, True))
 # stdout: (1, '1', (0, True))
 d = {}
 print(d)
+#stdout: {}
 # stdout: {}
 key1 = "k1"
 
@@ -114,12 +133,16 @@ def key2():
 
 d = {key1: "val1", key2(): "val2"}
 print(d)
+#stdout: {'k1': 'val1', 'key2': 'val2'}
 # stdout: {'k1': 'val1', 'key2': 'val2'}
 d = {"x": 0, "y": "something"}
 print(d)
+#stdout: {'x': 0, 'y': 'something'}
 # stdout: {'x': 0, 'y': 'something'}
 print(d["x"])
+#stdout: 0
 # stdout: 0
 d["z"] = True
 print(d)
+#stdout: {'x': 0, 'y': 'something', 'z': True}
 # stdout: {'x': 0, 'y': 'something', 'z': True}
